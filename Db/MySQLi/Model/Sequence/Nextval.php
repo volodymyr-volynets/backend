641f46829f6c9fcd44031372db6d269ed6488403
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2018-01-31 20:13:42 -0500

    fixed nextval functions for mysql

--- Db/MySQLi/Model/Sequence/Nextval.php
+++ Db/MySQLi/Model/Sequence/Nextval.php
@@ -9,6 +9,8 @@ class Nextval extends \Object\Function2 {
 	public $backend = 'MySQLi';
 	public $header = 'nextval(sequence_name varchar(255))';
 	public $definition = 'CREATE FUNCTION nextval(sequence_name varchar(255)) RETURNS BIGINT
+READS SQL DATA
+DETERMINISTIC
 BEGIN
 	UPDATE sm_sequences SET sm_sequence_counter = last_insert_id(sm_sequence_counter + 1) WHERE sm_sequence_name = sequence_name;
 	RETURN last_insert_id();
