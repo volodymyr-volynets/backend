ee05a8ac1d2025c00044f92b69f5b975afab2edf
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2020-02-16 18:14:03 -0500

    Backend:	Enhancement \#1333 \(Print templates model.\)

---
+++ System/Modules/Model/PrintTemplates.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Numbers\Backend\System\Modules\Model;
+class PrintTemplates extends \Object\Table {
+	public $db_link;
+	public $db_link_flag;
+	public $module_code = 'SM';
+	public $title = 'S/M Print Templates';
+	public $name = 'sm_print_templates';
+	public $pk = ['sm_printtemplate_code'];
+	public $tenant;
+	public $module;
+	public $orderby;
+	public $limit;
+	public $column_prefix = 'sm_printtemplate_';
+	public $columns = [
+		'sm_printtemplate_code' => ['name' => 'Code', 'domain' => 'group_code'],
+		'sm_printtemplate_type_code' => ['name' => 'Type', 'domain' => 'group_code'],
+		'sm_printtemplate_name' => ['name' => 'Name', 'domain' => 'name'],
+		'sm_printtemplate_activation_model' => ['name' => 'Activation Model', 'domain' => 'code', 'null' => true],
+		'sm_printtemplate_inactive' => ['name' => 'Inactive', 'type' => 'boolean']
+	];
+	public $constraints = [
+		'sm_print_templates_pk' => ['type' => 'pk', 'columns' => ['sm_printtemplate_code']],
+	];
+	public $indexes = [
+		'sm_print_templates_fulltext_idx' => ['type' => 'fulltext', 'columns' => ['sm_printtemplate_code', 'sm_printtemplate_name']]
+	];
+	public $history = false;
+	public $audit = false;
+	public $optimistic_lock = true;
+	public $options_map = [
+		'sm_printtemplate_name' => 'name',
+		'sm_printtemplate_code' => 'name',
+		'sm_printtemplate_inactive' => 'inactive'
+	];
+	public $options_active = [
+		'sm_printtemplate_inactive' => 0
+	];
+	public $engine = [
+		'MySQLi' => 'InnoDB'
+	];
+
+	public $cache = true;
+	public $cache_tags = [];
+	public $cache_memory = false;
+
+	public $data_asset = [
+		'classification' => 'public',
+		'protection' => 1,
+		'scope' => 'global'
+	];
+}
